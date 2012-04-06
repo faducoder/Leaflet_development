6ab10bcb70ab3d34efe5e685d15f740d709ebdbe
Spencer <spencecooley@gmail.com>
2012-04-06 17:00:54 -0500

    got marker drag events to work. When you drag you will see the change of latlng in the corner

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 
-var map = new L.Map('map');
-
-var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/d4334cd6077140e3b92ccfae2b363070/997/256/{z}/{x}/{y}.png', {
+var map, cloudmade, sanAntonio, polygonPoints  
+ 
+ 
+ map = new L.Map('map');
+ cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/d4334cd6077140e3b92ccfae2b363070/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 });
 
-var sanAntonio = new L.LatLng(29.4238889, -98.4933333); // geographical point (longitude and latitude)
-map.setView(sanAntonio, 12).addLayer(cloudmade);
+
+ sanAntonio = new L.LatLng(29.4238889, -98.4933333); // geographical point (longitude and latitude)
+
+
+ map.setView(sanAntonio, 13).addLayer(cloudmade);
 
 
 
@@ -16,16 +21,41 @@ map.setView(sanAntonio, 12).addLayer(cloudmade);
 polygonPoints = [];
 
 
-var polygon = new L.Polygon(polygonPoints);
-map.addLayer(polygon);
-
-
 map.on('click', function(e) {  
     
-    polygonPoints.push(e.latlng);
-    polygon.setLatLngs(polygonPoints);
-      
+  var marker = new L.Marker(e.latlng, {draggable:true});
+  polygonPoints.push(e.latlng);
+  var markerId = polygonPoints.length -1 
+  map.addLayer(marker);
+  
+  
+  
+  
+  
+  //I get undefined when I click the marker here 
+  marker.on('drag', function(){
+    var locationWhileDrag = marker.getLatLng();
+    $('#first_marker').val(locationWhileDrag);
+  });      
+
 });
+
+
+
+
+
+
+//this will create a polygon based on the polygonPoints array values 
+//var polygon = new L.Polygon(polygonPoints);
+//map.addLayer(polygon);
+
+
+//map.on('click', function(e) {  
+    
+//    polygonPoints.push(e.latlng);
+//    polygon.setLatLngs(polygonPoints);
+      
+//});
 
 
 
